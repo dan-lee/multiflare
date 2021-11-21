1375d3f3d8406021b732494403555a4dbe411378
Daniel Lehr <daniellehr@gmx.de>
2021-11-21 20:46:30 +0100

    Root worker test

--- src/rootWorker.ts
+++ src/rootWorker.ts
@@ -7,11 +7,12 @@ import matchUrl from 'match-url-wildcard'
 export default {
   async fetch(
     req: Request,
-    env: { routes: Record<string, string[]>; https: boolean },
+    env: { routes: Record<string, string[]>; https?: boolean },
   ) {
-    const project = Object.entries(env.routes).find(([, routes]) =>
-      routes.find((route) => matchUrl(req.url, route)),
-    )?.[0]
+    const [project] =
+      Object.entries(env.routes).find(([, routes]) =>
+        routes.find((route) => matchUrl(req.url, route)),
+      ) ?? []
 
     if (!project) {
       return
---
+++ test/rootWorker.test.ts
@@ -0,0 +1,31 @@
+import { jest } from '@jest/globals'
+import rootWorker from '../src/rootWorker'
+
+const fetchInterceptor = jest.fn()
+;(globalThis as any).fetch = fetchInterceptor
+
+beforeEach(fetchInterceptor.mockReset)
+
+describe('rootWorker', () => {
+  it('should match and fetch route', async () => {
+    // blog.example.test is routed to 127.0.0.1
+    const url = 'http://blog.example.test/news'
+    await rootWorker.fetch(new Request(url), {
+      routes: { blog: ['blog.example.test/*'] },
+    })
+
+    // still 127.0.0.1 replies (subdomain is not relevant) but with appended mount path
+    expect(fetchInterceptor.mock.calls[0][0]).toBe(
+      'http://blog.example.test/blog/news',
+    )
+  })
+
+  it('should do nothing if no route matches', async () => {
+    const url = 'http://i-do-not-exist.example.test/'
+    await rootWorker.fetch(new Request(url), {
+      routes: { api: ['api.example.test/*'] },
+    })
+
+    expect(fetchInterceptor).not.toHaveBeenCalled()
+  })
+})
